ac5c317cc8b361688a779103b51e0aa92001fc81
sami4thewin <samiomarmohammed@gmail.com>
2017-03-22 08:25:34 -0400

    Everything except for server tests passing

--- migrations/20160523121832_setup_blog.js
+++ migrations/20160523121832_setup_blog.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
       tbl.string('name');
       tbl.string('username').unique();
       tbl.string('email').unique();
+      tbl.string('password', 128);
       tbl.timestamps();
     }),
     knex.schema.createTableIfNotExists('posts', (tbl) => {
@@ -20,6 +21,11 @@ exports.up = function(knex, Promise) {
       tbl.integer('user_id').references('users.id');
       tbl.integer('post_id').references('posts.id');
       tbl.timestamps();
+    }),
+    knex.schema.createTableIfNotExists('users_users', (tbl) => {
+      tbl.integer('user_id').references('users.id');
+      tbl.integer('follower_id').references('users.id');
+      tbl.unique(['user_id', 'follower_id']);
     })
   ]);
 };
@@ -28,6 +34,6 @@ exports.down = function(knex, Promise) {
   return knex.schema
     .dropTable('comments')
     .dropTable('posts')
-    .dropTable('users');
+    .dropTable('users')
+    .dropTable('users_users');
 };
-
